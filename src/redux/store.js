326a9674fb8b9957612f97be4354d9040f1ba674
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-09-10 00:57:43 +0200

    sum

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import contactsReducer from './contactSlice';
-
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-  },
-});
