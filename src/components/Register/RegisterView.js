61a0c4c003f00edfe5c54aaec861ae7360bb2250
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-09-10 21:48:07 +0200

    sum

--- src/components/Register/RegisterView.js
+++ src/components/Register/RegisterView.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 
 export default function RegisterView() {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -19,9 +19,43 @@ export default function RegisterView() {
         return;
     }
   };
-}
 
-const handleSubmit = e => {
-  e.preventDefault();
-  // dispatch(auth)
-};
+  const handleSubmit = e => {
+    e.preventDefault();
+    // dispatch(authOperations.register({ name, email, password }));
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h1> Странница регистрации</h1>
+
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <label>
+          Name
+          <input type="text" name="name" value={name} onChange={handleChange} />
+        </label>
+        <label>
+          Email
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="text"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+      </form>
+    </div>
+  );
+}
