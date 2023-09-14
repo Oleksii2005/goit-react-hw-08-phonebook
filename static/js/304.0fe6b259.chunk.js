a3cc22173c00e5987617a9981b2f9be34ca47844
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-09-14 19:09:57 +0000

    Deploying to gh-pages from @ Oleksii2005/goit-react-hw-08-phonebook@75e8269deb265095d16579e625bf2a17aebf50cb 🚀

---
+++ static/js/304.0fe6b259.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[304],{6304:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var o={container:"Home_container__AHomM",title:"Home_title__qAdt+"},a=t(184);function c(){return(0,a.jsx)("div",{className:o.container,children:(0,a.jsx)("h1",{className:o.title,children:"Phonebook manager"})})}}}]);
+//# sourceMappingURL=304.0fe6b259.chunk.js.map
